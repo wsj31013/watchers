098a8d304dd050f1ae11046d48f711bd4e472b11
wooseongjin <travis31013@naver.com>
2019-05-19 17:26:11 +0900

    Add sys.argv for error of arguments

--- watchers.py
+++ watchers.py
@@ -1,6 +1,7 @@
 #!/Users/wooseongjin/.pyenv/shims/python
 from optparse import OptionParser
 import subprocess
+import sys
 
 # Alias of argrument
 nginx = "/var/log/nginx/access.log"
@@ -9,25 +10,31 @@ tomcat = "/var/lib/tomcat/logs/catalina.out"
 def main():
     usage = "usage: %prog -s arg1 -l arg2"
     parser = OptionParser(usage=usage, version="%prog 1.0")
-    parser.add_option('-s', '--server', dest='servername', type='string', help='target server host')
-    parser.add_option('-l', '--log', dest='logfile', type='string', help='logfile full path')
+    parser.add_option('-s', '--server', action='store', dest='servername', type='string', help='target server host')
+    parser.add_option('-l', '--log', action='store', dest='logfile', type='string', help='logfile full path')
     parser.set_defaults(verbose=True)
 
     (options, args) = parser.parse_args()
 
-    if options.servername and options.logfile and options.logfile in nginx and len(args) == 0:
+    if options.servername and options.logfile and options.logfile in nginx and len(args) == 0 and len(sys.argv) == 5:
         command = "ssh root@" + options.servername + " tail -f " + nginx
         # print(command)
         # print(len(args))
+        # print(type(args))
+        # print(len(sys.argv))
         subprocess.call(command, shell=True)
-    elif options.servername and options.logfile and options.logfile in tomcat and len(args) == 0:
+    elif options.servername and options.logfile and options.logfile in tomcat and len(args) == 0 and len(sys.argv) == 5:
         command = "ssh root@" + options.servername + " tail -f " + tomcat
         # print(command)
         # print(len(args))
         subprocess.call(command, shell=True)      
     elif options.servername and options.logfile and len(args) != 0:
+        # print(len(args))
+        # print(len(sys.argv))
         parser.error("Too many arguments")
     else:
+        # print(len(args))
+        # print(len(sys.argv))
         parser.error("Wrong number of arguments")
 
 
